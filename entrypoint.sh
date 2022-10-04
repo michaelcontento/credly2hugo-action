154fbe00e0c5dfa8545ea3ba3b21e0b97d31aaca
Michael Contento <mail@michaelcontento.de>
2022-10-04 07:23:18 +0200

    Fixing issues found by shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,50 +7,50 @@ echo "::debug::Credly username: ${INPUT_USERNAME}"
 INPUT_DATAFILE="${INPUT_DATAFILE:-$2}"
 INPUT_DATAFILE="${INPUT_DATAFILE:-credly-badges.json}"
 INPUT_DATAFILE="${GITHUB_WORKSPACE}/data/${INPUT_DATAFILE}"
-mkdir -p "$(dirname ${INPUT_DATAFILE})"
-rm -f "${INPUT_DATAFILE}"
-echo "::debug::Storage location for badge json file: ${INPUT_DATAFILE}"
+mkdir -p "$(dirname "$INPUT_DATAFILE")"
+rm -f "$INPUT_DATAFILE"
+echo "::debug::Storage location for badge json file: $INPUT_DATAFILE"
 
 INPUT_IMAGEDIR="${INPUT_IMAGEDIR:-$3}"
 INPUT_IMAGEDIR="${INPUT_IMAGEDIR:-assets/images/credly-badges}"
 INPUT_IMAGEDIR="${GITHUB_WORKSPACE}/${INPUT_IMAGEDIR}"
-mkdir -p "${INPUT_IMAGEDIR}"
-echo "::debug::Storage location for badge image files: ${INPUT_IMAGEDIR}"
+mkdir -p "$INPUT_IMAGEDIR"
+echo "::debug::Storage location for badge image files: $INPUT_IMAGEDIR"
 
 URL="https://www.credly.com/users/${INPUT_USERNAME}/badges.json"
-echo "::debug::Credly json url: ${URL}"
+echo "::debug::Credly json url: $URL"
 echo "::notice::Downloading infos from Credly"
-curl --silent $URL \
+curl --silent "$URL" \
     | jq --sort-keys '
-        .data 
+        .data
         | map({
             "description": .badge_template.description,
-            "expires_at": .expires_at_date?, 
-            "id": .id, 
-            "image_url": .badge_template.image_url, 
-            "issued_at": .issued_at_date, 
-            "issuer_url": .badge_template.global_activity_url,             
-            "microsoft_ids": 
-                [.badge_template.badge_template_activities[] | .url] 
-                | del(..|nulls) 
+            "expires_at": .expires_at_date?,
+            "id": .id,
+            "image_url": .badge_template.image_url,
+            "issued_at": .issued_at_date,
+            "issuer_url": .badge_template.global_activity_url,
+            "microsoft_ids":
+                [.badge_template.badge_template_activities[] | .url]
+                | del(..|nulls)
                 | [.[] | split("/") | .[-1] | ascii_upcase ],
-            "name": .badge_template.name, 
+            "name": .badge_template.name,
             "url": ("https://www.credly.com/badges/" + .id)
-        }) 
+        })
         | sort_by(.issued_at)' \
-    > "${INPUT_DATAFILE}"
+    > "$INPUT_DATAFILE"
 
-BADGED_FOUND=$(jq '.[] | .id' "${INPUT_DATAFILE}" | wc -l)
-echo "::notice::Found ${BADGED_FOUND} badges on Credly"
+BADGED_FOUND=$(jq '.[] | .id' "$INPUT_DATAFILE" | wc -l)
+echo "::notice::Found $BADGED_FOUND badges on Credly"
 
-for line in $(jq --raw-output '.[] | (.image_url + "," + .id)' ${INPUT_DATAFILE}); do
-    IFS=, read url id <<< $line
+for line in $(jq --raw-output '.[] | (.image_url + "," + .id)' "$INPUT_DATAFILE"); do
+    IFS=, read -r url id <<< "$line"
 
-    outputFile="${INPUT_IMAGEDIR}/${id}.${url##*.}"    
-    if [ -f $outputFile ]; then
-        echo "::notice::Badge image for id ${id} already present - skipping download"
+    outputFile="${INPUT_IMAGEDIR}/${id}.${url##*.}"
+    if [ -f "$outputFile" ]; then
+        echo "::notice::Badge image for id $id already present - skipping download"
     else
-        echo "::notice::Downloading bage image for id ${id}"
-        curl --silent "${url}" > $outputFile    
+        echo "::notice::Downloading bage image for id $id"
+        curl --silent "$url" > "$outputFile"
     fi
 done
