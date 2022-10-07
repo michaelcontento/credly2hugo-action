07968b03ea26fca3a48ecfc5162a2d210c37ed12
Michael Contento <mail@michaelcontento.de>
2022-10-07 22:08:15 +0200

    Use Hugo naming conventions for attributes in our data file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ INPUT_USERNAME="${INPUT_USERNAME:-$1}"
 echo "::debug::Credly username: ${INPUT_USERNAME}"
 
 INPUT_DATAFILE="${INPUT_DATAFILE:-$2}"
-INPUT_DATAFILE="${INPUT_DATAFILE:-credly-badges.json}"
+INPUT_DATAFILE="${INPUT_DATAFILE:-CredlyBadges.json}"
 INPUT_DATAFILE="${GITHUB_WORKSPACE}/data/${INPUT_DATAFILE}"
 mkdir -p "$(dirname "$INPUT_DATAFILE")"
 rm -f "$INPUT_DATAFILE"
 echo "::debug::Storage location for badge json file: $INPUT_DATAFILE"
 
 INPUT_IMAGEDIR="${INPUT_IMAGEDIR:-$3}"
-INPUT_IMAGEDIR="${INPUT_IMAGEDIR:-assets/images/credly-badges}"
+INPUT_IMAGEDIR="${INPUT_IMAGEDIR:-assets/images/CredlyBadges}"
 INPUT_IMAGEDIR="${GITHUB_WORKSPACE}/${INPUT_IMAGEDIR}"
 mkdir -p "$INPUT_IMAGEDIR"
 echo "::debug::Storage location for badge image files: $INPUT_IMAGEDIR"
@@ -24,18 +24,25 @@ curl --silent "$URL" \
     | jq --sort-keys '
         .data
         | map({
-            "description": .badge_template.description,
-            "expires_at": .expires_at_date?,
-            "id": .id,
-            "image_url": .badge_template.image_url,
-            "issued_at": .issued_at_date,
-            "issuer_url": .badge_template.global_activity_url,
-            "microsoft_ids":
+            "CredlyUrl": ("https://www.credly.com/badges/" + .id),
+            "Description": .badge_template.description,
+            "ExpiresAt": .expires_at_date?,
+            "Id": .id,
+            "IssuedAt": .issued_at_date,
+            "IssuerUrl": .badge_template.global_activity_url,
+            "LocalImagePath": (
+                "'$INPUT_IMAGEDIR'"
+                + "/"
+                + .id
+                + "."
+                + (.badge_template.image_url | split(".") | .[-1])
+            ),
+            "MicrosoftIds":
                 [.badge_template.badge_template_activities[] | .url]
                 | del(..|nulls)
                 | [.[] | split("/") | .[-1] | ascii_upcase ],
-            "name": .badge_template.name,
-            "url": ("https://www.credly.com/badges/" + .id)
+            "Name": .badge_template.name,
+            "RemoteImageUrl": .badge_template.image_url
         })
         | sort_by(.issued_at)' \
     > "$INPUT_DATAFILE"
@@ -43,14 +50,13 @@ curl --silent "$URL" \
 BADGED_FOUND=$(jq '.[] | .id' "$INPUT_DATAFILE" | wc -l)
 echo "::notice::Found $BADGED_FOUND badges on Credly"
 
-for line in $(jq --raw-output '.[] | (.image_url + "," + .id)' "$INPUT_DATAFILE"); do
-    IFS=, read -r url id <<< "$line"
+for line in $(jq --raw-output '.[] | (.Id + "," + .RemoteImageUrl + "," + .LocalImagePath)' "$INPUT_DATAFILE"); do
+    IFS=, read -r id remoteUrl localPath <<< "$line"
 
-    outputFile="${INPUT_IMAGEDIR}/${id}.${url##*.}"
-    if [ -f "$outputFile" ]; then
+    if [ -f "$localPath" ]; then
         echo "::notice::Badge image for id $id already present - skipping download"
     else
         echo "::notice::Downloading bage image for id $id"
-        curl --silent "$url" > "$outputFile"
+        curl --silent "$remoteUrl" > "$localPath"
     fi
 done
